7cdafad9f56a4ab4baee7d7e3af881cdbd1ff999
mykewayner <mykewayner@gmail.com>
2022-07-16 21:00:37 -0300

    Style: changed header media query

--- src/containers/Header/styles.js
+++ src/containers/Header/styles.js
@@ -5,7 +5,7 @@ export const Logo = styled.img`
         height: 50px;
       margin-top: 15px;
       margin-left: 15px;
-      @media(max-width: 650px){
+      @media(max-width: 700px){
         height: 60px;}
 `;
 
