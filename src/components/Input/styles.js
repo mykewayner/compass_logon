0c74faea9a33384ada707e0f79d92fbe716a0255
mykewayner <mykewayner@gmail.com>
2022-07-17 23:24:57 -0300

    Feat: created page title change

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 
 export const Inputs = styled.input`
     background-color: transparent;
-    border: solid 1px #e0e0e0ec;
+    border: solid 1px ${props => props.errorColor ? '#E9B425' : '#FFFFFF'}; 
     box-sizing: border-box;
     border-radius: 50px;
     color: #e0e0e0ec;
     padding-left: 20px;
     font-size: 1.2em;
     height: 50px;
+    margin: 10px 0px;
     width: 100%;
     
 
